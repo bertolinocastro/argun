46dc85a1be59bf26793ff42592315ebc3f53be59
bertolinocastro <jgabbc@hotmail.com>
2017-08-05 12:03:26 -0300

    Teste do Vuforia

--- Assets/Script/balcaoScript.cs
+++ Assets/Script/balcaoScript.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class balcaoScript : MonoBehaviour {
+public class balcaoScript : MonoBehaviour, ITrackableEventHandler {
 
 	public GameObject pino;
 	public Transform spawnPinoL;
 	public Transform spawnPinoR;
 
-
 	private  List<GameObject> pinos;
+
+	private TrackableBehaviour myTrack;
 
 	// Use this for initialization
 	void Start () {
 		pinos = new List<GameObject> ();
+
+		myTrack = GetComponent<TrackableBehaviour> ();
+		if (myTrack)
+			myTrack.RegisterTrackableEventHandler (this);
 	}
-	
-	// Update is called once per frame
-	void Update() {
-		
-		if (this.gameObject.activeSelf) {
+
+	public void OnTrackableStateChanged( TrackableBehaviour.Status oldS, TrackableBehaviour.Status newS ){
+		if ( newS == TrackableBehaviour.Status.DETECTED ||
+		     newS == TrackableBehaviour.Status.TRACKED ||
+		     newS == TrackableBehaviour.Status.EXTENDED_TRACKED) {
 			if (pinos.Count < 4) {
 				float xSpawn = spawnPinoL.transform.position.x;
 				float length = Mathf.Abs(spawnPinoL.transform.position.x - spawnPinoR.transform.position.x);
@@ -28,7 +34,7 @@ public class balcaoScript : MonoBehaviour {
 					pinos.Add (Instantiate<GameObject> (pino, 
 						new Vector3 (xSpawn + i * length/3, spawnPinoL.transform.position.y + 2.0f, spawnPinoL.transform.position.z),
 						Quaternion.identity,
-						this.transform));
+						this.transform.GetChild(0)));
 					pinos [i].transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
 				}
@@ -39,5 +45,9 @@ public class balcaoScript : MonoBehaviour {
 			}
 			pinos.Clear();
 		}
+	}
+
+	// Update is called once per frame
+	void Update() {
 	}
 }
