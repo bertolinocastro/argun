b01547157c8b6c59aa55c08f21e8556484751723
bertolinocastro <jgabbc@hotmail.com>
2017-08-05 16:05:54 -0300

    Finalizacao esqueleto padrao

--- Assets/Script/balcaoScript.cs
+++ Assets/Script/balcaoScript.cs
@@ -3,51 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class balcaoScript : MonoBehaviour, ITrackableEventHandler {
+public class balcaoScript : MonoBehaviour {
 
 	public GameObject pino;
 	public Transform spawnPinoL;
 	public Transform spawnPinoR;
 
 	private  List<GameObject> pinos;
-
-	private TrackableBehaviour myTrack;
+	private List<Vector3> pinosPos;
+	private Quaternion balcaoRotInit;
 
 	// Use this for initialization
 	void Start () {
+
+		// Travando rotacao
+		//transform.GetComponent<Rigidbody>().freezeRotation = true;
+		balcaoRotInit = transform.GetChild(0).transform.rotation;
+
 		pinos = new List<GameObject> ();
+		pinosPos = new List<Vector3> ();
 
-		myTrack = GetComponent<TrackableBehaviour> ();
-		if (myTrack)
-			myTrack.RegisterTrackableEventHandler (this);
-	}
+		float xSpawn = spawnPinoL.transform.position.x;
+		float length = Mathf.Abs(spawnPinoL.transform.position.x - spawnPinoR.transform.position.x);
 
-	public void OnTrackableStateChanged( TrackableBehaviour.Status oldS, TrackableBehaviour.Status newS ){
-		if ( newS == TrackableBehaviour.Status.DETECTED ||
-		     newS == TrackableBehaviour.Status.TRACKED ||
-		     newS == TrackableBehaviour.Status.EXTENDED_TRACKED) {
-			if (pinos.Count < 4) {
-				float xSpawn = spawnPinoL.transform.position.x;
-				float length = Mathf.Abs(spawnPinoL.transform.position.x - spawnPinoR.transform.position.x);
 
-				for (int i = 0; i < 4; i++) {
-					pinos.Add (Instantiate<GameObject> (pino, 
-						new Vector3 (xSpawn + i * length/3, spawnPinoL.transform.position.y + 2.0f, spawnPinoL.transform.position.z),
-						Quaternion.identity,
-						this.transform.GetChild(0)));
-					pinos [i].transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		for (int i = 0; i < 4; i++) {
+			pinosPos.Add (new Vector3 (xSpawn + i * length / 3, spawnPinoL.transform.position.y + 5.0f, spawnPinoL.transform.position.z));
 
-				}
-			}
-		} else {
-			foreach (var p in pinos) {
-				Destroy (p);
-			}
-			pinos.Clear();
+			pinos.Add (Instantiate<GameObject> (pino, 
+				pinosPos[i],
+				Quaternion.identity,
+				this.transform.GetChild (0)));
+			pinos [i].transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
 		}
 	}
 
 	// Update is called once per frame
 	void Update() {
+
+		transform.GetChild (0).transform.rotation = balcaoRotInit;
+
+		float xSpawn = spawnPinoL.transform.position.x;
+		float length = Mathf.Abs(spawnPinoL.transform.position.x - spawnPinoR.transform.position.x);
+
+		int i = 0; 
+		foreach (var p in pinos) {
+			if (p.transform.localPosition.x > 40.0f || p.transform.localPosition.z > 15.0f || p.transform.localPosition.z < -15.0f || p.transform.localPosition.y < - 20.0f) {
+				//p.transform.position = transform.GetChild (0).transform.GetChild (0).position + new Vector3( pinosPos [i].x, 0, 0 );
+				p.transform.position = new Vector3 (xSpawn + i * length / 3, spawnPinoL.transform.position.y + 5.0f, spawnPinoL.transform.position.z);
+				p.transform.rotation = Quaternion.identity;
+				p.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+				p.GetComponent<Rigidbody> ().angularVelocity= Vector3.zero;
+			}
+			i++;
+		}
 	}
 }
---
+++ Assets/bulletScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if( transform.position.z > 1000.0f || transform.position.y < -100.0f || 
+			transform.position.x < -200.0f || transform.position.x > 200.0f )
+			Destroy(gameObject);
+			
+			
+			/*
+		if (p.transform.localPosition.x > 40.0f || p.transform.localPosition.z > 15.0f
+			|| p.transform.localPosition.z < -15.0f || p.transform.localPosition.y < - 20.0f) {*/
+	}
+}
